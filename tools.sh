0df24b95f6fef3651538e75424e827a442925abc
sasdallas <sasdallas765@gmail.com>
2022-01-21 19:12:06 +0000

    Add first beta build and change some things

--- tools.sh
+++ tools.sh
@@ -3,9 +3,10 @@ sudo apt-get update
 
 echo Downloading build tools...
 
-sudo apt-get install -y binutils gcc nasm python3 perl 
+sudo apt-get install -y binutils gcc nasm python3 perl qemu qemu-system grub2-common
 
-echo Install i686-elf toolchain(NOT WORKING)...
-wget https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/i686-elf-tools-linux.zip
-unzip i686-elf-tools-linux.zip
+echo Install i686-elf toolchain...
+echo "nevermind we gonna skip that, doesn't work"
+#wget https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/i686-elf-tools-linux.zip
+#unzip i686-elf-tools-linux.zip
 echo Complete! Ready for build.
