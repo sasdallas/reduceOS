1dfc61280738b1a49be3aa3043ad3a4ac9c4ce1a
Samuel <40303843+sasdallas@users.noreply.github.com>
2022-11-25 13:28:10 -0600

    Update terminal.h to include stdarg and va_list

--- source/kernel/include/terminal.h
+++ source/kernel/include/terminal.h
@@ -9,6 +9,8 @@
 #include "include/libc/stdbool.h" // Boolean declarations
 #include "include/libc/string.h" // String functions
 #include "include/libc/limits.h" // Limits on integers and more.
+#include "include/libc/stdarg.h" // va argument handling (for ... on printf)
+#include "include/libc/va_list.h" // va_list declared here.
 #include "include/graphics.h" // Utility functions
 
 // Variable declarations
@@ -27,6 +29,7 @@ void updateTerminalColor(uint8_t color); // updateTerminalColor() - Change the t
 void terminalPutcharXY(unsigned char c, uint8_t color, size_t x, size_t y); // terminalPutcharXY() - Place an unsigned char at a certain X and Y. Not recommended, doesn't include scrolling stuff.
 void scrollTerminal(int line); // scrollTerminal() - I don't see how this could be used outside of terminal.c, but just in case. Scrolls a line down.
 void terminalDeleteLastLine(); // terminalDeleteLastLine() - Removes the last line placed by the terminal.
+void clearScreen(); // clearScreen() - clears the terminal screen.
 void terminalPutchar(char c); // terminalPutchar() - Recommended function. Incorporates scrollTerminal and terminalDeleteLastLine.
 void terminalWrite(const char *data, size_t size); // terminalWrite() - This nor terminalWriteString is recommended for use. Use printf. It prints data using a for loop, but needs length.
 void terminalWriteString(const char* data); // terminalWriteString() - The exact same as terminalWrite but with strlen() included.
