00aaa1c1ca0eed693f83b3f46d3584badc1a4db1
Samuel <40303843+sasdallas@users.noreply.github.com>
2022-11-26 14:12:05 -0600

    Add strcpy

--- source/kernel/include/libc/string.h
+++ source/kernel/include/libc/string.h
@@ -16,5 +16,6 @@ void* memcpy(void* __restrict, const void* __restrict, size_t); // memcpy() - co
 void* memmove(void*, const void*, size_t); // memmove() - memcpy but moving it instead of copying.
 size_t strlen(const char* len); // strlen() - Returns the length of a string.
 char* itoa(int num, char* buffer, int base); // itoa() - converts an integer to a string.
+char *strcpy(char *dest, const char *src); // strcpy() - copies one string to another
 
 #endif
