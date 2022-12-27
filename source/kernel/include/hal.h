b562526ff59611bb0ae09f4e6cec95787d319ac2
sasdallas <sasdallas765@gmail.com>
2022-12-27 09:27:04 -0600

    Add basic stack trace to panic functionality and remove argument parsing in command.c - will fix that later.

--- source/kernel/include/hal.h
+++ source/kernel/include/hal.h
@@ -24,5 +24,7 @@ extern void disableHardwareInterrupts(); // Disable hardware interrupts
 extern uint8_t inportb(uint16_t port); // Read data from a device using port mapped IO
 extern void outportb(uint16_t port, uint8_t value); // Write data to a device using port mapped IO
 extern void __cpuid(uint32_t type, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx); // Returns an assembly cpuid instruction.
+extern uint32_t inportl(uint16_t port); // Read data from a device via port mapped IO
+extern void outportl(uint16_t port, uint32_t data); // Write data to a device via port mapped IO
 
 #endif
