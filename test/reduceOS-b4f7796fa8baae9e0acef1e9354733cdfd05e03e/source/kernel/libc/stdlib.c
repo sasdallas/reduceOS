9086dbce9d00cd6e8bc402d4fff0a09c7a03a7b5
sasdallas <sasdallas765@gmail.com>
2023-02-25 20:24:40 -0600

    Remove leftover debug stuff

--- test/reduceOS-b4f7796fa8baae9e0acef1e9354733cdfd05e03e/source/kernel/libc/stdlib.c
+++
@@ -1,9 +0,0 @@
-// stdlib.c - replacement for standard C stdlib file. Contains certain useful functions like abs().
-// Why is this required? We use -ffreestanding in our compile options, so the standard std library isn't included.
-
-#include "include/libc/stdlib.h"
-
-int abs(int x) {
-    if (x < 0) x = -x;
-    return x;
-}
