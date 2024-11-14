060524dcc9eadb4f54a2b1ac43f12d9bdaf8a41c
Samuel <sasdallas765@gmail.com>
2024-11-13 19:23:04 -0600

    acpica: Initial source commit

---
+++ hexahedron/drivers/x86/acpica/hexahedron_acpica.c
@@ -0,0 +1,18 @@
+/**
+ * @file hexahedron/drivers/x86/acpica/hexahedron_acpica.c
+ * @brief ACPICA interface for Hexahedron
+ * 
+ * 
+ * 
+ * @copyright
+ * This file is part of the Hexahedron kernel, which is part of reduceOS.
+ * It is released under the terms of the BSD 3-clause license.
+ * Please see the LICENSE file in the main repository for more details.
+ * 
+ * ACPICA and its components are licensed under BSD license, the same as Hexahedron.
+ * ACPICA is created by Intel corporation.
+ * Licensing of ACPICA is available in hexahedron/drivers/x86/acpica/LICENSE
+ * 
+ * Copyright (C) 2024 Samuel Stuart
+ */
+
