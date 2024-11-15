8d89698453fcee97cced51064390ced214f3a737
Samuel <sasdallas765@gmail.com>
2024-11-14 21:45:12 -0600

    acpica: Closer to successful kernel->acpica linkage. Issues with vsnprintf, snprintf, sprintf in utprint.c and unimplemented AcpiOs functions in OS layer

--- hexahedron/drivers/x86/hexahedron_acpica.c
+++ hexahedron/drivers/x86/hexahedron_acpica.c
@@ -24,125 +24,60 @@
 #include <kernel/mem/mem.h>
 #include <kernel/mem/alloc.h>
 #include <kernel/misc/spinlock.h>
-#include <kernel/misc/semaphore.h>
-
-#include <stdarg.h>
-#include <errno.h>
 
 
 #define FUNC_UNIMPLEMENTED(FUNCTION)    \
-    LOG(WARN, "%s: Unimplemented\n", FUNCTION);    \
-    kernel_panic_extended(UNSUPPORTED_FUNCTION_ERROR, "ACPICA", "*** %s not implemented\n", FUNCTION);   \
+    dprintf(WARN, "ACPICA %s: Unimplemented\n", FUNCTION);    \
+    kernel_panic_extended(UNSUPPORTED_FUNCTION_ERROR, "acpica", "*** %s not implemented\n", FUNCTION);   \
     __builtin_unreachable();
 
-#define LOG(status, message, ...) dprintf_module(status, "ACPICA:OSL", message, ## __VA_ARGS__)
-
-/* Exposed to kernel */
-int ACPICA_Initialize() {
-    LOG(INFO, "ACPICA was compiled into kernel. Initializing ACPICA\n");
-    
-    ACPI_STATUS status;
-    
-    status = AcpiInitializeSubsystem();
-    if (ACPI_FAILURE(status)) {
-        LOG(ERR, "AcpiInitializeSubsystem did not succeed - status %i\n", status);
-        return -1; 
-    }
-
-    // Initialize tables
-    status = AcpiInitializeTables(NULL, 16, FALSE);
-    if (ACPI_FAILURE(status)) {
-        LOG(ERR, "AcpiInitializeTables did not succeed - status %i\n", status);
-        AcpiTerminate();
-        return -1;
-    }
-
-    // Load tables
-    status = AcpiLoadTables();
-    if (ACPI_FAILURE(status)) {
-        LOG(ERR, "AcpiLoadTables did not succeed - status %i\n", status);
-        AcpiTerminate();
-        return -1;
-    }
-
-    // Enable subsystem
-    status = AcpiEnableSubsystem(ACPI_FULL_INITIALIZATION);
-    if (ACPI_FAILURE(status)) {
-        LOG(ERR, "AcpiEnableSystem did not succeed - status %i\n", status);
-        AcpiTerminate();
-        return -1;
-    }
-
-    LOG(INFO, "Initialization completed successfully.\n");
-    return 0;
-}
-
-
-/* INITIALIZE/TERMINATE FUNCTIONS */
-
+/* Initialize */
 ACPI_STATUS AcpiOsInitialize() {
     return AE_OK;
 }
 
+/* Terminate */
 ACPI_STATUS AcpiOsTerminate() {
     return AE_OK;
 }
-typedef struct {
-    char signature[8];
-    uint8_t checksum;
-    char OEMID[6];
-    uint8_t revision;
-    uint32_t rsdtAddress;
-} __attribute__((packed)) RSDPDescriptor;
 
 /* Get the RSDP */
 ACPI_PHYSICAL_ADDRESS AcpiOsGetRootPointer() {
     // Try to ask HAL (loading from EFI provides RSDP in ST)
     ACPI_PHYSICAL_ADDRESS rsdp = (ACPI_PHYSICAL_ADDRESS)hal_getRSDP();
-
     if (rsdp == 0x0) {
-        ACPI_STATUS status = AcpiFindRootPointer(&rsdp); // Use ACPICA to find the pointer.
-        if (ACPI_FAILURE(status)) return 0x0;
+        AcpiFindRootPointer(&rsdp); // Use ACPICA to find the pointer.
     }
 
     return rsdp;
 }
 
-/* OVERRIDE FUNCTIONS */
-
+/* Override certain ACPI variables (unused) */
 ACPI_STATUS AcpiOsPredefinedOverride(const ACPI_PREDEFINED_NAMES *PredefinedObject, ACPI_STRING *NewValue) {
     *NewValue = NULL;
     return AE_OK;
 }
 
+/* Override certain ACPI tables (unused) */
 ACPI_STATUS AcpiOsTableOverride(ACPI_TABLE_HEADER *ExistingTable, ACPI_TABLE_HEADER **NewTable) {
     *NewTable = NULL;
     return AE_OK;
 }
 
-ACPI_STATUS AcpiOsPhysicalTableOverride(ACPI_TABLE_HEADER *ExistingTable, ACPI_PHYSICAL_ADDRESS *NewAddress, UINT32 *NewTableLength) {
-    *NewAddress = 0x0;
-    return AE_OK;
-}
-
-/* MEMORY FUNCTIONS */
-
+/* Map memory */
 void *AcpiOsMapMemory(ACPI_PHYSICAL_ADDRESS PhysicalAddress, ACPI_SIZE Length) {
     // just like pmm it
-    LOG(DEBUG, "AcpiOsMapMemory 0x%x 0x%x\n", PhysicalAddress, Length);
     return (void*)mem_remapPhys((uintptr_t)PhysicalAddress);
 }
 
 /* Unmap memory */
 void AcpiOsUnmapMemory(void *where, ACPI_SIZE Length) {
     // Considering we're returning PMM-mapped memory, nah.
-    LOG(DEBUG, "AcpiOsUnmapMemory 0x%x 0x%x\n", where, Length);
 }
 
 /* Get a physical address */
 ACPI_STATUS AcpiOsGetPhysicalAddress(void *LogicalAddress, ACPI_PHYSICAL_ADDRESS *PhysicalAddress) {
     // It is possible to just call mem_getPhysicalAddress as well
-    LOG(DEBUG, "AcpiOsGetPhysicalAddress 0x%x\n", LogicalAddress);
     *PhysicalAddress = (ACPI_PHYSICAL_ADDRESS)(LogicalAddress - (void*)MEM_IDENTITY_MAP_REGION);
     return AE_OK;
 }
@@ -167,14 +102,14 @@ BOOLEAN AcpiOsWritable(void *Memory, ACPI_SIZE Length) {
     FUNC_UNIMPLEMENTED("AcpiOsWritable");
 }
 
-/* UNIMPLEMENTED THREAD FUNCTIONS (SINGLE-THREADED) */
+/* UNIMPLEMENTED THREAD FUNCTIONS */
 
 ACPI_THREAD_ID AcpiOsGetThreadId() {
-    return 1; // Constant thread. According to ACPICA docs this should work.
+    FUNC_UNIMPLEMENTED("AcpiOsGetThreadId");
 }
 
 ACPI_STATUS AcpiOsExecute(ACPI_EXECUTE_TYPE Type, ACPI_OSD_EXEC_CALLBACK Function, void *Context) {
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsExecute");
 }
 
 void AcpiOsSleep(UINT64 Milliseconds) {
@@ -186,56 +121,27 @@ void AcpiOsStall(UINT32 Microseconds) {
     FUNC_UNIMPLEMENTED("AcpiOsStall");
 }
 
-void AcpiOsWaitEventsComplete() {
-    LOG(WARN, "Unimplemented AcpiOsWaitEventsComplete - not critical\n");
-}
 
 /* SEMAPHORE FUNCTIONS */
 
 /* Create a semaphore */
 ACPI_STATUS AcpiOsCreateSemaphore(UINT32 MaxUnits, UINT32 InitialUnits, ACPI_SEMAPHORE *OutHandle) {
-    *OutHandle = semaphore_create("acpica_sem", InitialUnits, MaxUnits);
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsCreateSemaphore");
 }
 
 /* Delete semaphore */
 ACPI_STATUS AcpiOsDeleteSemaphore(ACPI_SEMAPHORE Handle) {
-    semaphore_destroy((semaphore_t*)Handle);
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsDeleteSemaphore");
 }
 
 /* Wait on semaphore */
 ACPI_STATUS AcpiOsWaitSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units, UINT16 Timeout) {
-    if (Timeout == 0) {
-        // Don't wait at all? Just check if there are enough units
-        if ((UINT32)semaphore_getItems(Handle) >= Units) {
-            semaphore_wait(Handle, Units);
-            return AE_OK;
-        }
-
-        return AE_TIME;
-    }
-
-    time_t start_time = now(); // Start time in ms
-    UINT32 remaining = Units;
-    while (remaining > 0 && now() - start_time < Timeout) {
-        remaining -= semaphore_wait(Handle, remaining);
-    }
-
-    if (remaining > 0) {
-        // Timeout exceeded
-        semaphore_signal(Handle, Units - remaining);
-        return AE_TIME; 
-    }
-
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsWaitSemaphore");
 }
 
 /* Signal the semaphore */
 ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units) {
-    int added = semaphore_signal(Handle, Units);
-    if ((UINT32)added != Units) return AE_LIMIT;
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsSignalSemaphore");
 }
 
 /* LOCK FUNCTIONS */
@@ -263,198 +169,11 @@ void AcpiOsReleaseLock(ACPI_SPINLOCK Handle, ACPI_CPU_FLAGS Flags) {
 }
 
 /* INTERRUPT FUNCTIONS */
-/* Hexahedron uses a different IRQ handler style, so we have to use a handler */
-
-#define MAX_ACPI_INTERRUPT_HANDLERS 16
-
-// Store data & handlers
-ACPI_OSD_HANDLER ACPI_interruptHandlers[MAX_ACPI_INTERRUPT_HANDLERS];
-void *ACPI_interruptContext[MAX_ACPI_INTERRUPT_HANDLERS];
-
-int ACPICA_InterruptHandler(uint32_t exception_number, uint32_t int_number, registers_t *registers, extended_registers_t *extended) {
-    if (ACPI_interruptHandlers[int_number]) {
-        ACPI_interruptHandlers[int_number](ACPI_interruptContext[int_number]);
-    }
-
-    return 0;
-}
-
-
 
 ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 InterruptLevel, ACPI_OSD_HANDLER Handler, void *Context) {
-    // Install the data
-    if (InterruptLevel > MAX_ACPI_INTERRUPT_HANDLERS || Handler == NULL) {
-        return AE_BAD_PARAMETER;
-    }
-
-    if (ACPI_interruptHandlers[InterruptLevel]) {
-        return AE_ALREADY_EXISTS;
-    }
-
-    ACPI_interruptHandlers[InterruptLevel] = Handler;
-    ACPI_interruptContext[InterruptLevel] = Context;
-
-    
-    int r = hal_registerInterruptHandler(InterruptLevel, ACPICA_InterruptHandler);
-    if (r != 0) {
-        // This seems like a kernel fault
-        LOG(ERR, "hal_registerInterruptHandler(%i, 0x%x) returned %i\n", InterruptLevel, ACPICA_InterruptHandler, r);
-        return AE_ERROR;
-    }
-    return AE_OK;
+    FUNC_UNIMPLEMENTED("AcpiOsInstallInterruptHandler");
 }
-ACPI_STATUS AcpiOsRemoveInterruptHandler(UINT32 InterruptLevel, ACPI_OSD_HANDLER Handler) {
-    if (InterruptLevel >= MAX_ACPI_INTERRUPT_HANDLERS || Handler == NULL ) {
-        return AE_BAD_PARAMETER;
-    }
-
-    if (ACPI_interruptHandlers[InterruptLevel] != Handler) {
-        // Doesn't exist or wrong one
-        return AE_NOT_EXIST;
-    }
-
-    ACPI_interruptHandlers[InterruptLevel] = NULL;
-    ACPI_interruptContext[InterruptLevel] = NULL;
-    hal_unregisterInterruptHandler(InterruptLevel);
-    
-    return AE_OK;
+ACPI_STATUS AcpiOsRemoveInterruptHandler(UINT32 InterruptNumber, ACPI_OSD_HANDLER Handler) {
+    FUNC_UNIMPLEMENTED("AcpiOsRemoveInterruptHandler");
 }
 
-/* LOGGING */
-
-/* 
- * NOTE: Most logs come in with a header (e.g. "Firmware Error (ACPI)") that is in AcpiOsPrintf,
- * and then error content is put into AcpiOsVprintf
- */
-void AcpiOsPrintf(const char *Format, ...) {
-    va_list ap;
-    va_start(ap, Format);
-    dprintf_va("ACPICA", NOHEADER, (char*)Format, ap);
-    va_end(ap);
-}
-
-void AcpiOsVprintf(const char *Format, va_list Args) {
-    dprintf_va("ACPICA", NOHEADER, (char*)Format, Args);
-}
-
-/* MORE MEMORY */
-
-ACPI_STATUS AcpiOsReadMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 *Value, UINT32 Width) {
-
-    void *ptr = (void*)mem_remapPhys((uintptr_t)Address);
-
-    switch (Width) {
-        case 8:
-            *Value = *(UINT8*)ptr; 
-            break;
-        case 16:
-            *Value = *(UINT16*)ptr; 
-            break;
-        case 32:
-            *Value = *(UINT32*)ptr; 
-            break;
-        case 64:
-            *Value = *(UINT64*)ptr; 
-            break;
-        default:
-            kernel_panic_extended(KERNEL_BAD_ARGUMENT_ERROR, "ACPICA", "*** AcpiOsReadMemory received bad width argument 0x%x\n", Width);
-    }
-    return AE_OK;
-}
-
-ACPI_STATUS AcpiOsWriteMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 Value, UINT32 Width) {
-    void *ptr = (void*)mem_remapPhys((uintptr_t)Address);
-
-    switch (Width) {
-        case 8:
-            *(UINT8*)ptr = Value;
-            break;
-        case 16:
-            *(UINT16*)ptr = Value;
-            break;
-        case 32:
-            *(UINT32*)ptr = Value;
-            break;
-        case 64:
-            *(UINT64*)ptr = Value;
-            break;
-        default:
-            kernel_panic_extended(KERNEL_BAD_ARGUMENT_ERROR, "ACPICA", "*** AcpiOsReadMemory received bad width argument 0x%x\n", Width);
-    }
-
-    return AE_OK;
-}
-
-
-/* I/O FUNCTIONS */
-
-ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address, UINT32 *Value, UINT32 Width) {
-    switch (Width) {
-        case 8:
-            *Value = inportb(Address);
-            break;
-        case 16:
-            *Value = inportw(Address);
-            break;
-        case 32:
-            *Value = inportl(Address);
-            break;
-        default:
-            kernel_panic_extended(KERNEL_BAD_ARGUMENT_ERROR, "ACPICA", "*** AcpiOsReadPort received bad width argument 0x%x\n", Width);
-    }
-
-    return AE_OK;
-}
-
-ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS Address, UINT32 Value, UINT32 Width) {
-    switch (Width) {
-        case 8:
-            outportb(Address, Value);
-            break;
-        case 16:
-            outportw(Address, Value);
-            break;
-        case 32:
-            outportl(Address, Value);
-            break;
-        default:
-            kernel_panic_extended(KERNEL_BAD_ARGUMENT_ERROR, "ACPICA", "*** AcpiOsReadPort received bad width argument 0x%x\n", Width);
-    }
-
-    return AE_OK;
-}
-
-/* (UNIMPLEMENTED) PCI FUNCTIONS */
-
-ACPI_STATUS AcpiOsReadPciConfiguration(ACPI_PCI_ID *PciId, UINT32 Register, UINT64 *Value, UINT32 Width) {
-    FUNC_UNIMPLEMENTED("AcpiOsReadPciConfiguration");
-    return AE_OK;
-}
-
-ACPI_STATUS AcpiOsWritePciConfiguration(ACPI_PCI_ID *PciId, UINT32 Register, UINT64 Value, UINT32 Width) {
-    FUNC_UNIMPLEMENTED("AcpiOsWritePciConfiguration");
-    return AE_OK;
-}
-
-/* MISC. */
-
-UINT64 AcpiOsGetTimer() {
-    return now() * 10 * 1000;
-}
-
-ACPI_STATUS AcpiOsSignal(UINT32 Function, void *Info) {
-    /* It might send other stuff but we need to catch fatal signals */
-    switch (Function) {
-        case ACPI_SIGNAL_FATAL:
-            ACPI_SIGNAL_FATAL_INFO *error_info = Info;
-            kernel_panic_extended(ACPI_SYSTEM_ERROR, "ACPICA", "*** ACPI AML error: Fatal error detected. Type: 0x%x Code: 0x%x Argument: 0x%x", 
-                    error_info->Type,
-                    error_info->Code,
-                    error_info->Argument);
-            break;
-        default:
-            LOG(DEBUG, "ACPI AML signal 0x%x received\n", Function);
-    }
-
-    return AE_OK;
-}
