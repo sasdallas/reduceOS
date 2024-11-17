45dc19bfea03ea1976f400242600e266e6ec3756
Samuel <sasdallas765@gmail.com>
2024-11-17 09:45:31 -0600

    libpolyhedron: Fix bad strncmp

--- libpolyhedron/string/strncmp.c
+++ libpolyhedron/string/strncmp.c
@@ -13,18 +13,15 @@
 
 #include <string.h>
 
-int strncmp(const char* str1, const char* str2, size_t size) {
-    if (!size) return 0;
+int strncmp(const char *s1, const char *s2, size_t n) {
+	if (n == 0) return 0;
 
-    while (*str1 == *str2) {
-        if (!size || !*str1) break; // Terminate if nothing left
-
-        str1++;
-        str2++;
-        size--;
-    }
-
-    return *(unsigned char*)str1 - *(unsigned char*)str2;
+	while (n-- && *s1 == *s2) {
+		if (!n || !*s1) break;
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1) - (*(unsigned char *)s2);
 }
 
 int strcmp(const char* str1, const char* str2) {
