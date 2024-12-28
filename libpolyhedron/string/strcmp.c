ec0eb0b86237f279279a2db3c9fa1676c0c7fba9
Samuel <sasdallas765@gmail.com>
2024-12-28 15:39:04 -0600

    libpolyhedron: strcmp bugfix

--- libpolyhedron/string/strcmp.c
+++ libpolyhedron/string/strcmp.c
@@ -14,7 +14,7 @@
 #include <string.h>
 
 int strcmp(const char* str1, const char* str2) {
-    while (*str1 == *str2) {
+    while (*str1 && *str2 && *str1 == *str2) {
         str1++;
         str2++;
     }
