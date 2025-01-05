6fca77b6761bccafb57fe84a2f7926c2e874be0a
Samuel <sasdallas765@gmail.com>
2025-01-05 10:52:48 -0600

    libpolyhedron: (arch:x86_64) Implement memcpy with arch-specific improvements

---
+++ libpolyhedron/arch/x86_64/string/memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * @file libpolyhedron/arch/x86_64/stdlib/memcpy.c
+ * @brief memcpy
+ * 
+ * 
+ * @copyright
+ * This file is part of the Hexahedron kernel, which is part of reduceOS.
+ * It is released under the terms of the BSD 3-clause license.
+ * Please see the LICENSE file in the main repository for more details.
+ * 
+ * Copyright (C) 2024 Samuel Stuart
+ */
+
+#include <stddef.h>
+
+void * memcpy(void * restrict dest, const void * restrict src, size_t n) {
+	asm volatile("cld; rep movsb"
+	            : "=c"((int){0})
+	            : "D"(dest), "S"(src), "c"(n)
+	            : "flags", "memory");
+	return dest;
+}
