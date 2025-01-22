403722b3fa5c5e03e94a53718155571e96ac53e9
Samuel <40303843+sasdallas@users.noreply.github.com>
2025-01-22 15:22:15 -0600

    Update ahci.c

--- source/kmods/ahci/ahci.c
+++ source/kmods/ahci/ahci.c
@@ -104,14 +104,11 @@ static void find_ahci(uint32_t device, uint16_t vendorID, uint16_t deviceID, voi
     uint16_t command_reg = pciConfigReadField(device, PCI_OFFSET_COMMAND, 2);
     command_reg |= (1 << 2);    // Turn on the bus master bit 
     command_reg |= (1 << 1);    // Turn on the memory space
-    command_reg ^= (1 << 10);   // This is useless, it's but the interrupt disable bit will either be set/unset. This doesn't change it.
+    // command_reg ^= (1 << 10);
     pciConfigWriteField(device, PCI_OFFSET_COMMAND, 2, command_reg); 
 
     // Print out some debug information
     serialPrintf("[module ahci] PCI interrupt line = %d\n", pciGetInterrupt(device));
-
-
-    serialPrintf("[module ahci] BAR5 = %#x\n", pciConfigReadField(device, PCI_OFFSET_BAR5, 4));
 
     // Use the memory subsystem to map regions
     uint32_t addr = pciConfigReadField(device, PCI_OFFSET_BAR5, 4) & 0xFFFFFFF0;
@@ -128,10 +125,9 @@ static void find_ahci(uint32_t device, uint16_t vendorID, uint16_t deviceID, voi
     // Read the AHCI version (debug)
     uint32_t ahciVersion = mem->vs;
     serialPrintf("[module ahci] Controller version %d.%d%d\n", (ahciVersion >> 16) & 0xFFF, (ahciVersion >> 8) & 0xFF, (ahciVersion) & 0xFF);
-    printf("[module ahci] AHCI controller loaded - controller version %d.%d%d\n", (ahciVersion >> 16) & 0xFFF, (ahciVersion >> 8) & 0xFF, (ahciVersion) & 0xFF); // debug code for a system I'm testing
 
-
-    // ahci_probePorts(mem);
+    // Broken
+    ahci_probePorts(mem);
 }
 
 
