e73ed91c033d16d00f9d4a9e0279edae83dc9d4d
Samuel <sasdallas765@gmail.com>
2025-04-06 13:44:35 -0500

    hexahedron: (task:fd) Initial commit

--- hexahedron/include/kernel/task/fd.h
+++ hexahedron/include/kernel/task/fd.h
@@ -14,4 +14,72 @@
 #ifndef KERNEL_TASK_FD_H
 #define KERNEL_TASK_FD_H
 
+/**** INCLUDES ****/
+#include <stdint.h>
+#include <kernel/arch/arch.h>
+#include <kernel/misc/spinlock.h>
+#include <kernel/mem/mem.h>
+#include <kernel/fs/vfs.h>
+
+/**** DEFINITIONS ****/
+
+#define PROCESS_FD_BASE_AMOUNT      8
+#define PROCESS_FD_EXPAND_AMOUNT    8
+
+/**** TYPES ****/
+
+/**
+ * @brief A single file descriptor. Located in a process' @c fd_table
+ */
+typedef struct fd {
+    int fd_number;              // File descriptor number
+    fs_node_t *node;            // File that this file descriptor is connected to
+    mode_t mode;                // Mode of file descriptor
+    uint64_t offset;            // Offset of file descriptor
+} fd_t;
+
+/**
+ * @brief File descriptor table
+ * @todo    Use a bitmap to keep track of free/used file descriptors? Right now the solution is sort of janky.
+ *          We can also just loop through to check for a NULL entry.
+ */
+typedef struct fd_table {
+    fd_t **fds;                 // List of expanding file descriptors
+    size_t amount;              // Amount of used file descriptors
+    size_t total;               // Total space for file descriptors allocated
+    size_t references;          // References by other processes
+    spinlock_t lock;            // Lock
+} fd_table_t;
+
+/**** MACROS ****/
+
+#define FD(proc, fd) (proc->fd_table->fds[fd])
+#define FD_VALIDATE(proc, fd) (proc->fd_table->amount >= (size_t)fd && proc->fd_table->fds[fd])
+
+/**** FUNCTIONS ****/
+
+/**
+ * @brief Destroy a file descriptor table for a process
+ * @param process Process the process to destroy the fd table for
+ * @returns 0 on success
+ */
+int fd_destroyTable(struct process *process);
+
+/**
+ * @brief Add a file descriptor for a process
+ * @param process The process to add the file descriptor to
+ * @param node The node to add the file descriptor for
+ * @returns A pointer to the file descriptor (for reference - it is already added to the process)
+ */
+fd_t *fd_add(struct process *process, fs_node_t *file);
+
+/**
+ * @brief Destroy a file descriptor for a process
+ * @param process The process to take the file descriptor from
+ * @param fd The file descriptor to remove
+ * @returns 0 on success
+ */
+int fd_remove(struct process *process, int fd_number);
+
+
 #endif
--- hexahedron/task/fd.c
+++ hexahedron/task/fd.c
@@ -13,3 +13,83 @@
 
 #include <kernel/task/process.h>
 #include <kernel/task/fd.h>
+#include <kernel/mem/alloc.h>
+#include <string.h>
+
+/**
+ * @brief Destroy a file descriptor table for a process
+ * @param process Process the process to destroy the fd table for
+ * @returns 0 on success
+ */
+int fd_destroyTable(struct process *process) {
+    spinlock_acquire(&process->fd_table->lock);
+    // Does the process fd table still have any references?
+    if (process->fd_table->references > 1) {
+        // Yes. Decrement and NULL
+        process->fd_table->references--;
+        spinlock_release(&process->fd_table->lock);
+        process->fd_table = NULL;
+        return 0;
+    }
+
+    // No, we can free this now.
+    // For each file descriptor we'll free the memory
+    for (size_t i = 0; i < process->fd_table->amount; i++) {
+        fd_t *fd = process->fd_table->fds[i];
+        if (fd) {
+            fs_close(fd->node);
+            kfree(fd);
+        }
+    }
+
+    kfree(process->fd_table->fds);
+    spinlock_release(&process->fd_table->lock);
+    kfree(process->fd_table);
+    process->fd_table = NULL;
+
+    return 0;
+}
+
+/**
+ * @brief Add a file descriptor for a process
+ * @param process The process to add the file descriptor to
+ * @param node The node to add the file descriptor for
+ * @returns A pointer to the file descriptor (for reference - it is already added to the process)
+ * 
+ * @note You should increment the file's refcount yourself
+ */
+fd_t *fd_add(struct process *process, fs_node_t *node) {
+    if (!process || !node) return NULL;
+
+    spinlock_acquire(&process->fd_table->lock);
+
+    // First, make sure the process' has enough file descriptors
+    if (process->fd_table->total <= process->fd_table->amount) {
+        // Reallocate to a new capacity, adding PROCESS_FD_EXPAND_AMOUNT
+        process->fd_table->total += PROCESS_FD_EXPAND_AMOUNT;
+        process->fd_table->fds = krealloc(process->fd_table->fds, sizeof(fd_t*) * process->fd_table->total);
+    }
+
+    // TODO: Search through the file descriptor list to find a better spot. This sucks.
+
+    // Allocate a new fd
+    fd_t *new_fd = kmalloc(sizeof(fd_t));
+    memset(new_fd, 0, sizeof(fd_t));
+    new_fd->fd_number = process->fd_table->amount;
+    new_fd->node = node;
+    process->fd_table->fds[process->fd_table->amount] = new_fd;
+    process->fd_table->amount++;
+
+    spinlock_release(&process->fd_table->lock);
+    return new_fd;   
+}
+
+/**
+ * @brief Destroy a file descriptor for a process
+ * @param process The process to take the file descriptor from
+ * @param fd The file descriptor to remove
+ * @returns 0 on success
+ */
+int fd_remove(struct process *process, int fd_number) {
+    return 0;
+}
