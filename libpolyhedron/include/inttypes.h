424d590e2d8f54e0e6543cb4f74b60847c9d685e
Samuel <sasdallas765@gmail.com>
2025-04-07 17:42:58 -0500

    libpolyhedron: (include) inttypes.h stub header file

---
+++ libpolyhedron/include/inttypes.h
@@ -0,0 +1,25 @@
+/**
+ * @file libpolyhedron/include/inttypes.h
+ * @brief inttypes
+ * 
+ * 
+ * @copyright
+ * This file is part of the Hexahedron kernel, which is part of reduceOS.
+ * It is released under the terms of the BSD 3-clause license.
+ * Please see the LICENSE file in the main repository for more details.
+ * 
+ * Copyright (C) 2024 Samuel Stuart
+ */
+
+#include <sys/cheader.h>
+
+_Begin_C_Header
+
+#ifndef _INTTYPES_H
+#define _INTTYPES_H
+
+
+
+#endif
+
+_End_C_Header
